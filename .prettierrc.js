7b8aaf01fcea6003f1f5a0824e394485b2231043
Dalia Abbruciati <d.abbruciati@campus.uniurb.it>
2024-07-29 19:36:53 +0200

    Initial setup

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  plugins: ['prettier-plugin-tailwindcss'],
+  printWidth: 80,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'es5',
+  useTabs: false,
+}
